c4f7e72c039aea35c27fd607746beae3e04da49b
Haruan Justino <haruanm@gmail.com>
2015-08-13 20:37:07 -0300

    first commit

--- upline_server/settings.py
+++ upline_server/settings.py
@@ -81,6 +81,7 @@ WSGI_APPLICATION = 'upline_server.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+DATABASES = []
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
