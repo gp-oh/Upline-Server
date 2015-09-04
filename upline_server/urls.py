6c821f834b02dccf83872227caead76cb9fff7fc
Haruan Justino <haruanm@gmail.com>
2015-09-04 16:35:39 -0300

    atualização de dependências

--- upline_server/urls.py
+++ upline_server/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(admin.site.urls)),
     url(r'^api/v1/oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^',include('upline.urls',namespace='upline')),
     url(r'^autocomplete/', include('autocomplete_light.urls')),
