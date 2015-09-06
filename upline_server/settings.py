ddb5d668203cee2fd0ed728682b5d60c58424881
Haruan Justino <haruanm@gmail.com>
2015-09-06 01:16:11 -0300

    adição de arquivos estáticos para jelastic

--- upline_server/settings.py
+++ upline_server/settings.py
@@ -89,7 +89,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Static asset configuration
 # STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_local')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Parse database configuration from $DATABASE_URL
