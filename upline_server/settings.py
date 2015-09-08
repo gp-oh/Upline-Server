7d13b46df87342721a3b6402bec37f5bab3f9349
Haruan Justino <haruanm@gmail.com>
2015-09-07 22:21:58 -0300

    database correta

--- upline_server/settings.py
+++ upline_server/settings.py
@@ -110,28 +110,28 @@ STATIC_URL = '/static/'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'upline',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'upline',
-#         'USER': 'upline',
-#         'PASSWORD': 'batatinhafrita123',
-#         'HOST': 'upline.c5rmgxifqspm.us-east-1.rds.amazonaws.com',
-#         'PORT': '5432',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'upline',
+        'USER': 'upline',
+        'PASSWORD': 'batatinhafrita123',
+        'HOST': 'upline.c5rmgxifqspm.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
