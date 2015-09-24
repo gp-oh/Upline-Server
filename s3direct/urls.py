14c48177de97040247549956d437344b7f178d78
Haruan Justino <haruanm@gmail.com>
2015-09-23 21:35:27 -0300

    Atualização s3direct

---
+++ s3direct/urls.py
@@ -0,0 +1,8 @@
+ # -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+from s3direct.views import get_upload_params
+
+urlpatterns = patterns('',
+                       url('^get_upload_params/',
+                           get_upload_params, name='s3direct'),
+                       )
