339cf28509624745d59081cc6ea63292df505f79
Haruan Justino <haruanm@gmail.com>
2015-12-15 14:56:23 -0200

    tab bugfix.

--- upline/views/api.py
+++ upline/views/api.py
@@ -61,7 +61,7 @@ class MemberViewSet(viewsets.ModelViewSet):
 
     def list(self, request):
         # if "only_children" in request.GET and len(request.GET['only_children']) == 1 and request.GET['only_children'] == 0:
-            queryset = Member.objects.get(user=request.user).get_descendants()
+        queryset = Member.objects.get(user=request.user).get_descendants()
         # else:
         #     queryset = Member.objects.filter(parent__user=request.user)
 
