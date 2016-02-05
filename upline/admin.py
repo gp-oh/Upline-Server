546a3bf3274427603bc9607629664df4f86ec234
Haruan Justino <haruanm@gmail.com>
2016-02-05 16:21:50 -0200

    linted admin changed notification to be sent at same day

--- upline/admin.py
+++ upline/admin.py
@@ -1,4 +1,4 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import datetime
 from django.contrib import admin
 from django.utils.translation import ugettext as _
@@ -6,18 +6,16 @@ from upline.models import *
 from upline.forms import *
 from django_mptt_admin.admin import DjangoMpttAdmin
 from django_extensions.admin import ForeignKeyAutocompleteAdmin
-from django.conf import settings
-from django.contrib.admin.util import flatten_fieldsets
 from django.contrib.admin import SimpleListFilter
-from upline.quickblox import create_user, delete_user
+from upline.quickblox import delete_user
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from django.contrib import admin
 from solo.admin import SingletonModelAdmin
 from upline.serializers import *
 
+
 class TrainingAdmin(admin.ModelAdmin):
-    list_display = ['id','name','notified']
+    list_display = ['id', 'name', 'notified']
     search_fields = ['name']
     actions = ['send']
 
@@ -25,25 +23,29 @@ class TrainingAdmin(admin.ModelAdmin):
         for instance in queryset:
             devices = GCMDevice.objects.filter()
             if len(devices) > 0:
-                devices.send_message(SiteConfiguration.get_solo().new_training_message , extra={"type":"training","object":OnlyTrainingSerializer(instance, many=False, context={'request': request}).data})
+                devices.send_message(SiteConfiguration.get_solo().new_training_message, extra={
+                                     "type": "training", "object": OnlyTrainingSerializer(instance, many=False, context={'request': request}).data})
         queryset.update(notified=True)
 
     send.short_description = u"Enviar notificações"
-    
+
+
 class LevelAdmin(admin.ModelAdmin):
     form = LevelForm
-    list_display = ['id','title','points_range_from','points_range_to']
+    list_display = ['id', 'title', 'points_range_from', 'points_range_to']
     search_fields = ['title']
 
-class MemberAdmin(ForeignKeyAutocompleteAdmin,DjangoMpttAdmin):
+
+class MemberAdmin(ForeignKeyAutocompleteAdmin, DjangoMpttAdmin):
     form = MemberForm
-    list_display = ['id',"user","parent","name","points","phone","gender","level","get_acoes"]
+    list_display = ['id', "user", "parent", "name",
+                    "points", "phone", "gender", "level", "get_acoes"]
     list_display_links = ['id']
     search_fields = ["name"]
     # actions = ['delete_model']
     related_search_fields = {
-       'user': ['first_name', 'email'],
-       'parent': ['name'],
+        'user': ['first_name', 'email'],
+        'parent': ['name'],
     }
 
     def has_delete_permission(self, request, obj=None):
@@ -74,25 +76,29 @@ class MemberAdmin(ForeignKeyAutocompleteAdmin,DjangoMpttAdmin):
             return self.readonly_fields + ('quickblox_id',)
         return self.readonly_fields
 
-    def get_acoes(self,obj):
-        return '<a href="/upline/member/'+str(obj.id)+'/linear/">linear</a> <a href="/upline/member/'+str(obj.id)+'/binary/">binário</a> <a href="/upline/member/'+str(obj.id)+'/">editar</a>'
+    def get_acoes(self, obj):
+        return '<a href="/upline/member/' + str(obj.id) + '/linear/">linear</a> <a href="/upline/member/' + str(obj.id) + '/binary/">binário</a> <a href="/upline/member/' + str(obj.id) + '/">editar</a>'
 
     get_acoes.short_description = 'Ações'
     get_acoes.allow_tags = True
 
+
 class Invited(Member):
+
     class Meta:
         proxy = True
 
+
 class InvitedAdmin(admin.ModelAdmin):
     form = MemberForm
-    list_display = ['id',"user","parent","name","points","phone","gender"]
+    list_display = ['id', "user", "parent",
+                    "name", "points", "phone", "gender"]
     list_display_links = ['id']
     search_fields = ['name']
     actions = ['delete_model']
     related_search_fields = {
-       'user': ('first_name', 'email'),
-       'parent': ('name'),
+        'user': ('first_name', 'email'),
+        'parent': ('name'),
     }
 
     def get_actions(self, request):
@@ -125,14 +131,15 @@ class InvitedAdmin(admin.ModelAdmin):
             return self.readonly_fields + ('quickblox_id',)
         return self.readonly_fields
 
+
 class ContactAdmin(ForeignKeyAutocompleteAdmin):
-    list_display = ['id',"owner","name","state","city","phone","member"]
+    list_display = ['id', "owner", "name", "state", "city", "phone", "member"]
     list_display_links = ['id']
     search_fields = ['name']
 
     related_search_fields = {
-       'owner': ('name'),
-       'member': ('name'),
+        'owner': ('name'),
+        'member': ('name'),
     }
 
     def get_related_filter(self, model, request):
@@ -149,9 +156,12 @@ class ContactAdmin(ForeignKeyAutocompleteAdmin):
             qs = qs.order_by(*ordering)
         return qs.filter(contact_category=0)
 
+
 class Client(Contact):
+
     class Meta:
         proxy = True
+
 
 class SaleInline(admin.TabularInline):
     model = Sale
@@ -168,8 +178,9 @@ class SaleInline(admin.TabularInline):
             [field.name for field in self.opts.local_many_to_many]
         ))
 
+
 class ClientAdmin(ForeignKeyAutocompleteAdmin):
-    list_display = ['id',"owner","name","state","city","phone","member"]
+    list_display = ['id', "owner", "name", "state", "city", "phone", "member"]
     list_display_links = ['id']
     search_fields = ['name']
 
@@ -178,8 +189,8 @@ class ClientAdmin(ForeignKeyAutocompleteAdmin):
     ]
 
     related_search_fields = {
-       'owner': ('name'),
-       'member': ('name'),
+        'owner': ('name'),
+        'member': ('name'),
     }
 
     def get_related_filter(self, model, request):
@@ -196,9 +207,10 @@ class ClientAdmin(ForeignKeyAutocompleteAdmin):
             qs = qs.order_by(*ordering)
         return qs.filter(contact_category=1)
 
+
 class GoalAdmin(ForeignKeyAutocompleteAdmin):
     related_search_fields = {
-       'member': ('name'),
+        'member': ('name'),
     }
 
     def get_related_filter(self, model, request):
@@ -208,8 +220,10 @@ class GoalAdmin(ForeignKeyAutocompleteAdmin):
             )
         return Q(member_type=0)
 
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id','name','active','points','table_value']
+    list_display = ['id', 'name', 'active', 'points', 'table_value']
+
 
 class SaleItemInline(admin.TabularInline):
     model = SaleItem
@@ -226,16 +240,17 @@ class SaleItemInline(admin.TabularInline):
             [field.name for field in self.opts.local_many_to_many]
         ))
 
+
 class SaleAdmin(ForeignKeyAutocompleteAdmin):
-    list_display = ['id','member', 'client','create_time','total']
-    
+    list_display = ['id', 'member', 'client', 'create_time', 'total']
+
     inlines = [
         SaleItemInline,
     ]
 
     related_search_fields = {
-       'member': ('name'),
-       'client': ('name'),
+        'member': ('name'),
+        'client': ('name'),
     }
 
     def get_related_filter(self, model, request):
@@ -250,7 +265,7 @@ class SaleAdmin(ForeignKeyAutocompleteAdmin):
         extra_context['readonly'] = True
         return super(SaleAdmin, self).change_view(request, object_id, extra_context=extra_context)
 
-    def has_delete_permission(self, request, x = None):
+    def has_delete_permission(self, request, x=None):
         return False
 
     def has_add_permission(self, request):
@@ -262,57 +277,65 @@ class SaleAdmin(ForeignKeyAutocompleteAdmin):
             [field.name for field in self.opts.local_many_to_many]
         ))
 
+
 class CityAdmin(admin.ModelAdmin):
-    list_display = ['id','state', 'name']
+    list_display = ['id', 'state', 'name']
     search_fields = ['name']
     list_filter = ['state']
 
+
 class StateAdmin(admin.ModelAdmin):
-    list_display = ['id','acronym', 'name']
+    list_display = ['id', 'acronym', 'name']
     search_fields = ['acronym', 'name']
+
 
 class PostalCodeAdmin(ForeignKeyAutocompleteAdmin):
     related_search_fields = {
-       'city': ('name'),
+        'city': ('name'),
     }
-    list_display = ['id','get_state','city','postal_code']
-    search_fields = ['postal_code','city']
+    list_display = ['id', 'get_state', 'city', 'postal_code']
+    search_fields = ['postal_code', 'city']
     list_filter = ['city__state']
-    
+
     def get_state(self, obj):
         return obj.city.state
 
     get_state.short_description = 'State'
     get_state.admin_order_field = 'city__state'
 
+
 class EventDateFilter(SimpleListFilter):
     title = _('Data')
     parameter_name = 'begin_time'
+
     def lookups(self, request, model_admin):
         return (
             ('PREVIOUS', _('Previous')),
             ('AFTER', _('After')),
         )
+
     def queryset(self, request, queryset):
         if self.value() == 'PREVIOUS':
             return queryset.filter(begin_time__lte=datetime.datetime.now())
         else:
             return queryset.filter(begin_time__gte=datetime.datetime.now())
 
+
 class EventAdmin(ForeignKeyAutocompleteAdmin):
     form = EventForm
-    list_display = ["id","title","calendar","begin_time"]
+    list_display = ["id", "title", "calendar", "begin_time"]
     search_fields = ['name']
     list_filter = [EventDateFilter]
     related_search_fields = {
-       'owner': ['username'],
-       'calendar': ['name'],
-       'invited': ['name'],
-       'members': ['name'],
+        'owner': ['username'],
+        'calendar': ['name'],
+        'invited': ['name'],
+        'members': ['name'],
     }
 
     def get_queryset(self, request):
-        qs = super(EventAdmin, self).get_queryset(request).filter(is_invited=False)
+        qs = super(EventAdmin, self).get_queryset(
+            request).filter(is_invited=False)
         ordering = self.get_ordering(request)
         if ordering:
             qs = qs.order_by(*ordering)
@@ -326,19 +349,23 @@ class EventAdmin(ForeignKeyAutocompleteAdmin):
             )
         return Q(member_type=0)
 
+
 class PostAdmin(ForeignKeyAutocompleteAdmin):
-    list_display = ['user','title','content','create_time','media_type','get_media_file','notified']
+    list_display = ['user', 'title', 'content', 'create_time',
+                    'media_type', 'get_media_file', 'notified']
     form = PostForm
     related_search_fields = {
-       'user': ('first_name', 'email'),
+        'user': ('first_name', 'email'),
     }
     actions = ['send']
 
     def send(modeladmin, request, queryset):
         for instance in queryset:
-            devices = GCMDevice.objects.filter(user__groups__in=instance.groups.all())
+            devices = GCMDevice.objects.filter(
+                user__groups__in=instance.groups.all())
             if len(devices) > 0:
-                devices.send_message(SiteConfiguration.get_solo().new_post_message , extra={"type":"post","object":PostSerializer(instance, many=False,context={'request': request}).data})
+                devices.send_message(SiteConfiguration.get_solo().new_post_message, extra={
+                                     "type": "post", "object": PostSerializer(instance, many=False, context={'request': request}).data})
         queryset.update(notified=True)
 
     def get_readonly_fields(self, request, obj=None):
@@ -348,27 +375,30 @@ class PostAdmin(ForeignKeyAutocompleteAdmin):
 
     def get_media_file(self, obj):
         if obj.media_type == 0:
-            return u'<img src="'+unicode(obj.media)+'" style="width:300px"/>'
+            return u'<img src="' + unicode(obj.media) + '" style="width:300px"/>'
         elif obj.media_type == 1:
-            return u'<audio src="'+unicode(obj.media)+'" controls>Your browser does not support the <code>audio</code> element.</audio>'
+            return u'<audio src="' + unicode(obj.media) + '" controls>Your browser does not support the <code>audio</code> element.</audio>'
         elif obj.media_type == 2:
-            return u'<video style="width:300px" controls><source src="'+unicode(obj.media)+'" type="video/mp4">Seu navegador nao suporta o elemento <code>video</code>.</video>'
+            return u'<video style="width:300px" controls><source src="' + unicode(obj.media) + '" type="video/mp4">Seu navegador nao suporta o elemento <code>video</code>.</video>'
         elif obj.media_type == 3:
             return ''
         else:
-            return u'<a target="_blank" class="pdf" href="'+unicode(obj.media)+'">'+unicode(obj.name)+'</a>'
+            return u'<a target="_blank" class="pdf" href="' + unicode(obj.media) + '">' + unicode(obj.name) + '</a>'
 
     get_media_file.short_description = 'Arquivo'
     get_media_file.allow_tags = True
     send.short_description = u"Enviar notificações"
 
+
 class CalendarAdmin(ForeignKeyAutocompleteAdmin):
     related_search_fields = {
-       'user': ('first_name', 'email'),
+        'user': ('first_name', 'email'),
     }
 
+
 class MediaAdmin(admin.ModelAdmin):
-    list_display = ['id','get_media_type','media_category','name','get_media_file','notified']
+    list_display = ['id', 'get_media_type', 'media_category',
+                    'name', 'get_media_file', 'notified']
     search_fields = ['name']
     list_filter = ['media_type']
     actions = ['send']
@@ -377,7 +407,8 @@ class MediaAdmin(admin.ModelAdmin):
         for instance in queryset:
             devices = GCMDevice.objects.filter()
             if len(devices) > 0:
-                devices.send_message(SiteConfiguration.get_solo().new_media_message , extra={"type":"media","object":MediaSerializer(instance, many=False,context={'request': request}).data})
+                devices.send_message(SiteConfiguration.get_solo().new_media_message, extra={
+                                     "type": "media", "object": MediaSerializer(instance, many=False, context={'request': request}).data})
         queryset.update(notified=True)
 
     def get_readonly_fields(self, request, obj=None):
@@ -390,13 +421,13 @@ class MediaAdmin(admin.ModelAdmin):
 
     def get_media_file(self, obj):
         if obj.media_type == 0:
-            return u'<img src="'+obj.media+'" style="width:300px"/>'
+            return u'<img src="' + obj.media + '" style="width:300px"/>'
         elif obj.media_type == 1:
-            return u'<audio src="'+obj.media+'" controls>Your browser does not support the <code>audio</code> element.</audio>'
+            return u'<audio src="' + obj.media + '" controls>Your browser does not support the <code>audio</code> element.</audio>'
         elif obj.media_type == 2:
-            return u'<video style="width:300px" controls><source src="'+unicode(obj.media)+'" type="video/mp4">Seu navegador nao suporta o elemento <code>video</code>.</video>'
+            return u'<video style="width:300px" controls><source src="' + unicode(obj.media) + '" type="video/mp4">Seu navegador nao suporta o elemento <code>video</code>.</video>'
         else:
-            return u'<a target="_blank" class="pdf" href="'+obj.media+'">'+obj.name+'</a>'
+            return u'<a target="_blank" class="pdf" href="' + obj.media + '">' + obj.name + '</a>'
 
     get_media_file.short_description = 'Arquivo'
     get_media_file.allow_tags = True
@@ -404,12 +435,14 @@ class MediaAdmin(admin.ModelAdmin):
     get_media_type.admin_order_field = 'media_category__media_type'
     send.short_description = u"Enviar notificações"
 
+
 class MediaCategoryAdmin(admin.ModelAdmin):
-    list_display = ['id','name']
+    list_display = ['id', 'name']
     search_fields = ['name']
 
+
 class TrainingStepAdmin(admin.ModelAdmin):
-    list_display = ["step","training","title","description"]
+    list_display = ["step", "training", "title", "description"]
     search_fields = ['name']
     list_filter = ['training']
     form = TrainingStepForm
@@ -419,9 +452,10 @@ class TrainingStepAdmin(admin.ModelAdmin):
             return self.readonly_fields + ('converted',)
         return self.readonly_fields
 
+
 class InviteAdmin(admin.ModelAdmin):
-    search_fields = ['member','name','email']
-    list_display = ['id','member','name','email','create_time']
+    search_fields = ['member', 'name', 'email']
+    list_display = ['id', 'member', 'name', 'email', 'create_time']
     change_list_template = 'admin/invite_list.html'
 
     def has_edit_permission(self, request, obj=None):
@@ -446,10 +480,10 @@ class InviteAdmin(admin.ModelAdmin):
 
 
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ['id','level','message','sent','get_date_sent']
+    list_display = ['id', 'level', 'message', 'sent', 'get_date_sent']
     actions = ['send']
 
-    def get_date_sent(self,obj):
+    def get_date_sent(self, obj):
         if obj.sent:
             return obj.update_time
         else:
@@ -459,7 +493,8 @@ class NotificationAdmin(admin.ModelAdmin):
 
     def send(modeladmin, request, queryset):
         for notification in queryset:
-            devices = GCMDevice.objects.filter(user__in=notification.level.user_set.all())
+            devices = GCMDevice.objects.filter(
+                user__in=notification.level.user_set.all())
             devices.send_message(notification.message)
         queryset.update(sent=True)
 
@@ -468,6 +503,7 @@ class NotificationAdmin(admin.ModelAdmin):
 
 class AvatarInline(admin.StackedInline):
     model = Avatar
+
 
 class MyUserAdmin(UserAdmin):
 
@@ -481,37 +517,37 @@ class MyUserAdmin(UserAdmin):
             del actions['delete_selected']
         return actions
 
-    def get_readonly_fields(self, request, obj = None):
-        if obj: #In edit mode
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # In edit mode
             return ('username',) + self.readonly_fields
         return self.readonly_fields
 
     def has_delete_permission(self, request, obj=None):
-       if obj is None:
-           return True
-       else:
-           return False
+        if obj is None:
+            return True
+        else:
+            return False
 
 admin.site.register(SiteConfiguration, SingletonModelAdmin)
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
-admin.site.register(Invite,InviteAdmin)
-admin.site.register(Invited,InvitedAdmin)
-admin.site.register(Notification,NotificationAdmin)
-admin.site.register(TrainingStep,TrainingStepAdmin)
-admin.site.register(Training,TrainingAdmin)
-admin.site.register(Level,LevelAdmin)
-admin.site.register(Member,MemberAdmin)
-admin.site.register(Contact,ContactAdmin)
-admin.site.register(Client,ClientAdmin)
-admin.site.register(Goal,GoalAdmin)
-admin.site.register(Product,ProductAdmin)
-admin.site.register(Sale,SaleAdmin)
-admin.site.register(City,CityAdmin)
-admin.site.register(State,StateAdmin)
-admin.site.register(PostalCode,PostalCodeAdmin)
-admin.site.register(Event,EventAdmin)
-admin.site.register(Post,PostAdmin)
-admin.site.register(Calendar,CalendarAdmin)
-admin.site.register(Media,MediaAdmin)
-admin.site.register(MediaCategory,MediaCategoryAdmin)
+admin.site.register(Invite, InviteAdmin)
+admin.site.register(Invited, InvitedAdmin)
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(TrainingStep, TrainingStepAdmin)
+admin.site.register(Training, TrainingAdmin)
+admin.site.register(Level, LevelAdmin)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Goal, GoalAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Sale, SaleAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(State, StateAdmin)
+admin.site.register(PostalCode, PostalCodeAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Calendar, CalendarAdmin)
+admin.site.register(Media, MediaAdmin)
+admin.site.register(MediaCategory, MediaCategoryAdmin)
--- upline/management/commands/send_notifications.py
+++ upline/management/commands/send_notifications.py
@@ -13,14 +13,17 @@ class Command(BaseCommand):
     def notify_late_tranings(self):
         last_training_steps = MemberTrainingStep.objects.all().order_by('member__id', '-training_step__training__position',
                                                                         '-training_step__step').distinct('member__id')
+        print last_training_steps
         tz = pytz.timezone(settings.TIME_ZONE)
         for training_step in last_training_steps:
             next_training_step = training_step.training_step.next_training_step()
             if next_training_step and next_training_step.have_notifications:
                 time_distance = (tz.localize(datetime.datetime.today(), is_dst=None).astimezone(pytz.utc) -
                                  training_step.update_time).days
+                print 'dias'
+                print time_distance
                 message = ''
-                if time_distance == 1:
+                if time_distance <= 1:
                     message = next_training_step.day_1_notification_description
                 elif time_distance == 2:
                     message = next_training_step.day_2_notification_description
@@ -57,7 +60,7 @@ class Command(BaseCommand):
                              member.create_time).days
 
             message = ''
-            if time_distance == 1:
+            if time_distance <= 1:
                 message = next_training_step.day_1_notification_description
             elif time_distance == 2:
                 message = next_training_step.day_2_notification_description
@@ -78,7 +81,7 @@ class Command(BaseCommand):
 
             devices = GCMDevice.objects.filter(
                 user=member.user)
-            if len(devices) > 0:
+            if len(devices) > 0 and message != '':
                 devices.send_message(message, extra={
                                      "type": "training_step_task", "object": TrainingStepTaskSerializer(next_training_step, many=False).data})
 
