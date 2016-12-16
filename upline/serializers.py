3976f643e451db14a810bd57e49d06bd19d65bf6
Juliano Gouveia <juliano.gouveia@olist.com>
2016-12-16 01:55:18 -0200

    Add cpf to member serializer

--- upline/serializers.py
+++ upline/serializers.py
@@ -284,7 +284,7 @@ class MemberSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Member
         fields = ("id", "descendant_count", 'email', "today_descendant_count", "binary", "downline_count", "member_type", "user", "avatar_base64", "dream1_base64", "dream2_base64", "avatar_base64", 'quickblox_id', 'parent',
-                  'downlines', 'create_time', 'external_id', 'name', 'points', 'avatar', 'phone', 'gender', 'postal_code', 'city', 'state', 'address', 'address_number', 'dream1', 'dream2', 'status', 'level', 'answers', 'birthday')
+                  'downlines', 'create_time', 'external_id', 'name', 'points', 'avatar', 'phone', 'gender', 'postal_code', 'city', 'cpf', 'state', 'address', 'address_number', 'dream1', 'dream2', 'status', 'level', 'answers', 'birthday')
 
 
 class MemberNotificationSerializer(serializers.HyperlinkedModelSerializer):
